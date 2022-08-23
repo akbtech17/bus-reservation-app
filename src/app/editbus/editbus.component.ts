f97f140777e0a3324ba37e6763100de61214f1e6
Jitin20 <jitinsharma2001@rediffmail.com>
2022-08-24 02:22:27 +0530

    Added logic for editbus in component.ts

--- src/app/editbus/editbus.component.ts
+++ src/app/editbus/editbus.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BusserviceService } from '../busservice.service';
+import { Ibus } from '../ibus';
+
 
 @Component({
   selector: 'app-editbus',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editbus.component.css']
 })
 export class EditbusComponent implements OnInit {
-
-  constructor() { }
+  busdata: Ibus = {
+    BusNo: 0,
+    Rows: 0,
+    Cols: 0,
+    DTime: '',
+    Atime: '',
+    Pickup: '',
+    SeatCost: 0,
+    DriverName: '',
+    DriverContact: 0,
+    TypeOfBus: '',
+  }
+  id:number = 0
+  constructor(private busservice:BusserviceService, private activatedroute:ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    const tid = this.activatedroute.snapshot.paramMap.get('id')
+    this.id = Number(tid)
+    this.busservice.getBus(this.id).subscribe((data:Ibus)=>{this.busdata=data})
   }
-
+saveBus(bus:Ibus){
+  this.busdata = bus
+  this.busservice.editBus(this.busdata).subscribe(
+    ()=>{
+      alert("Records Edited")
+      this.router.navigate(['/list/'])
+    }
+  )
+}
 }
