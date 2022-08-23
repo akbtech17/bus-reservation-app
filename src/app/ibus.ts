d9289c7f94b411a86ca6b3a44a8bc4a56080c632
Jitin20 <jitinsharma2001@rediffmail.com>
2022-08-23 23:05:14 +0530

    Entered attributes in Interface

--- src/app/ibus.ts
+++ src/app/ibus.ts
@@ -1,2 +1,12 @@
 export interface Ibus {
+    BusNo: number
+    Rows: number
+    Cols: number
+    DTime: string
+    ATime: string
+    Pickup: string
+    SeatCost: number
+    DriverName: string
+    DriverContact: number
+    TypeOfBus: string
 }
