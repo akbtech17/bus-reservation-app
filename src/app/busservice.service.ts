d960c44ffbadd73d749555831a3459a3ce9608f9
Jitin20 <jitinsharma2001@rediffmail.com>
2022-08-23 23:35:17 +0530

    Added method for listbus. ->Display all the attributes using get.

--- src/app/busservice.service.ts
+++ src/app/busservice.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Ibus } from './ibus'; //importing interface
+import {HttpClient, HttpHeaders} from '@angular/common/http' //hhtp client object helps with all WEBApi methods
+import { Observable } from 'rxjs'; // to work or load single component not whole page or app.
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BusserviceService {
 
-  constructor() { }
+  url = 'http://localhost:30263/api/bus'
+  httpOptions = {headers: new HttpHeaders({'Content-type':'application/json'})}
+
+  constructor(private httpclient:HttpClient) { }
+
+  //To get All the attributes of Bus Controller
+  getBusList():Observable<any>{
+    return this.httpclient.get<any[]>(this.url+'/listbus')
+  }
+
 }
