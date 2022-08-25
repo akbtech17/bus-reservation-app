9a139da4112d21233a036801042673850d10a453
anshul bansal <akbt.tech17@gmail.com>
2022-08-26 01:00:07 +0530

    fixed the query string to body request for validate admin api

--- BusReservationApi/Controllers/AdminController.cs
+++ BusReservationApi/Controllers/AdminController.cs
@@ -5,6 +5,10 @@ using System.Linq;
 
 namespace BusReservationApi.Controllers
 {
+    public class Credentials {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
     public class Admin {
         public int AdminId { get; set; }
         public string Email { get; set; }
@@ -54,11 +58,11 @@ namespace BusReservationApi.Controllers
 
         [HttpPost]
         [Route("validate")]
-        public IActionResult PostAdmin(string Email, string Password) {
+        public IActionResult PostAdmin(Credentials creds) {
             if (ModelState.IsValid) {
                 try
                 {
-                    var data = db.Admins.Where(admin => admin.Email == Email && admin.Password == Password).FirstOrDefault(); 
+                    var data = db.Admins.Where(admin => admin.Email == creds.Email && admin.Password == creds.Password).FirstOrDefault(); 
                     if (data != null) return Ok(data);
                     else return NotFound("Admin is not registered");
                 }
