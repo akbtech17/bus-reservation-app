dcede53ad350bcecc460c59e8e528075ece0d347
anshul bansal <akbt.tech17@gmail.com>
2022-08-25 15:58:30 +0530

    fix : namespaces

--- BusReservationApi/Startup.cs
+++ BusReservationApi/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace BusAPI
+namespace BusReservationApi
 {
     public class Startup
     {
