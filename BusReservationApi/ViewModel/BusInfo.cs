9b4ff08e05e7773870b418de4baa6561ac47e954
anshul bansal <akbt.tech17@gmail.com>
2022-08-25 16:53:48 +0530

    created new view model

---
+++ BusReservationApi/ViewModel/BusInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace BusReservationApi.ViewModel
+{
+    public class BusInfo
+    {
+        public int BusId { get; set; }
+        public string BusNo { get; set; }
+        public int Rows { get; set; }
+        public int Cols { get; set; }
+        public string DTime { get; set; }
+        public string Atime { get; set; }
+        public int? RouteId { get; set; }
+        public string Pickup { get; set; }
+        public int SeatCost { get; set; }
+        public string DriverName { get; set; }
+        public string DriverContact { get; set; }
+        public string TypeOfBus { get; set; }
+        public string Source { get; set; }
+        public string? Destination { get; set; }
+        public int Distance { get; set; }
+    }
+}
