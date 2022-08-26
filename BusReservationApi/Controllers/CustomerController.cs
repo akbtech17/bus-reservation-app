8265ed9f46a5635ae785dfe07589b7457a590743
anshul bansal <akbt.tech17@gmail.com>
2022-08-26 13:58:22 +0530

    omkar : added 6 apis for customer

---
+++ BusReservationApi/Controllers/CustomerController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusReservationApi.Models;
+
+
+
+namespace BusReservationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        BusReservationContext db = new BusReservationContext();
+        [HttpGet]
+        public IActionResult GetCust()
+        {
+            try
+            {
+                var data = db.Customers.ToList();
+                return Ok(data);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException.Message);
+            }
+        }
+        [HttpGet]
+        [Route("{CustomerId}")]
+        public IActionResult GetCust(int CustomerId)
+        {
+            try
+            {
+                var data = db.Customers.Where(cust => cust.CustomerId == CustomerId).FirstOrDefault();
+                return Ok(data);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException.Message);
+            }
+        }
+        [HttpPost]
+        [Route("validate")]
+        public IActionResult PostCust(Credentials creds)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var data = db.Customers.Where(cust => cust.Email == creds.Email && cust.Password == creds.Password).FirstOrDefault();
+                    if (data != null) return Ok(data);
+                    else return NotFound("Customer is not registered");
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.InnerException.Message);
+                }
+            }
+            return BadRequest("Something went wrong");
+        }
+        [HttpPut]
+        [Route("{CustomerId}")]
+        public IActionResult PutCust(int CustomerId, Customer Cust)
+        {
+            var data = db.Customers.Where(cust => cust.CustomerId == CustomerId).FirstOrDefault();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    data.FirstName = Cust.FirstName;
+                    data.LastName = Cust.LastName;
+                    data.Dob = Cust.Dob;
+                    data.Gender = Cust.Gender;
+                    data.Mobile = Cust.Mobile;
+                    data.Email = Cust.Email;
+                    data.Password = Cust.Password;
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.InnerException.Message);
+            }
+            db.SaveChanges();
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult PostCust(Customer Cust)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Customers.Add(Cust);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.InnerException.Message);
+            }
+            db.SaveChanges();
+            return Created("Record Successfully Added", Cust);
+        }
+
+
+
+        [HttpDelete]
+        [Route("{CustomerId}")]
+        public IActionResult DeleteCust(int CustomerId)
+        {
+            try
+            {
+                var data = db.Customers.Where(cust => cust.CustomerId == CustomerId).FirstOrDefault();
+                db.Customers.Remove(data);
+                db.SaveChanges();
+                return Ok("Successfully deleted the Customer");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.InnerException.Message);
+            }
+        }
+    }
+}
