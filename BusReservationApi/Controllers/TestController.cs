e2224893317ba23f2c5f6a1e7f3f35684d1fa900
anshul bansal <akbt.tech17@gmail.com>
2022-08-28 03:48:02 +0530

    added apis and procedure to set our database with hardcoded values as soon as we launch the backend

---
+++ BusReservationApi/Controllers/TestController.cs
@@ -0,0 +1,46 @@
+﻿using BusReservationApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BusReservationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        BusReservationContext db = new BusReservationContext();
+
+        [HttpDelete]
+        [Route("deletedata")]
+        public IActionResult DeleteAllData()
+        {
+            try
+            {
+                db.Database.ExecuteSqlInterpolated($"TestDeleteAllRecords");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.InnerException.Message);
+            }
+            return Ok("Your DataBase is Clean Now!");
+        }
+
+        [HttpPost]
+        [Route("adddata")]
+        public IActionResult InsertAllData()
+        {
+            try
+            {
+                DeleteAllData();
+                db.Database.ExecuteSqlInterpolated($"TestInsertAllRecords");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.InnerException.Message);
+            }
+            return Ok("Your DataBase is UpToDate Now!");
+        }
+    }
+}
