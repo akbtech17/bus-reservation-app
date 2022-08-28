fd2dae92fdf167ca5a90edd830077cc0f8f26b18
Jitin20 <jitinsharma2001@rediffmail.com>
2022-08-29 03:05:47 +0530

    Working on Seat Selection.

--- src/app/seatbook/seatbook.component.ts
+++ src/app/seatbook/seatbook.component.ts
@@ -14,20 +14,35 @@ export class SeatbookComponent implements OnInit {
   seatdata: any[]=[]
   cols:number[]=[1,2,3,4,5]
   busId:number = 0
-  selected:[]=[]
-  select = false
-  status = 'disable'
-  tabIndex = 0
-  onSelect(){
-    this.select=!this.select;
-    this.status = this.select?'disable':'enable';
-    console.log("Selected")
-  }
-  onClick(index:number){
-    this.tabIndex = index
-  }
+  msg:string=''
+  tbIndex = 0
+  id=0
+clickEvent(index:number,id:number)
+{
+  this.tbIndex=index
+  index=id
+  
+  this.msg = this.seatdata[id].seatNo
+  return this.msg
+}
+ isSelected = false;
+  handleClick($event: MouseEvent):void{
+    this.isSelected = !this.isSelected;
+}
 
-  // seatmap: any[][] = []
+
+  // select = false
+  // status = 'disable'
+  // tabIndex = 0
+  // onSelect(){
+  //   this.select=!this.select;
+  //   this.status = this.select?'disable':'enable';
+  //   console.log("Selected")
+  // }
+  // onClick(index:number){
+  //   this.tabIndex = index
+  // }
+   seatmap: any[][] = []
   // rseat:any[] = []
   // cnt: number = 0
   // row: number = 0
@@ -39,25 +54,16 @@ export class SeatbookComponent implements OnInit {
     const tid = this.activatedroute.snapshot.paramMap.get('busId')
     this.busId = Number(tid)
     this.seatservice.getseats(this.busId).subscribe(
-      (data:any[])=>
+      (data:[])=>
       {this.seatdata = data, console.log(this.seatdata,"Value from callback")}
       
     )
-    console.log(this.seatdata)
-
-    // for(var i=0; i<5; i++) {
-    //   for(var j=0; j<4; j++) {
-    //     this.rseat.push(this.seatdata[this.cnt])
-    //     this.cnt++
-    //   }
-    //   this.seatmap[this.row].push(this.rseat)
-    //   this.rseat = []  
-    //   this.row++;  
-    // }
-    // console.log(this.seatmap)
    
-
+    
+      
   } 
+ 
+ 
 }
   
 
