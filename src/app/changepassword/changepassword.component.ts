c36c3fc650931916fa322faee09c4a1ddfc964bd
anshul bansal <akbt.tech17@gmail.com>
2022-09-01 02:49:47 +0530

    component and validations are done for change password

--- src/app/changepassword/changepassword.component.ts
+++ src/app/changepassword/changepassword.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TransactionDetails } from '../transaction-details';
 
 @Component({
   selector: 'app-changepassword',
@@ -7,16 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangepasswordComponent implements OnInit {
   changePassQuery : any  = {
-    customerId: 0
-    
+    customerId: 0,
+    oldPassword:'',
+    newPassword:''
   }
+  confirmPass: string = ''
   constructor() { }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
+    this.changePassQuery.oldPassword = TransactionDetails.customerId
+    this.changePassQuery.oldPassword = TransactionDetails.password
+    if(this.confirmPass != this.changePassQuery.newPassword) {
+      alert("Entered passwords do not match")
+      return
+    }
+    if(TransactionDetails.password == this.changePassQuery.newPassword) {
+      alert("New Password is same as your Old Password, Try some new combination!")
+      return
+    }
+    console.log(this.changePassQuery)
     alert("Password Changed Successfuly")
   }
-
 }
