557a682a1fb37209eb35c8fa824a78f660af561d
anshul bansal <akbt.tech17@gmail.com>
2022-09-01 03:22:03 +0530

    cleanup

--- src/app/seatservice.service.ts
+++ src/app/seatservice.service.ts
@@ -4,10 +4,10 @@ import {HttpHeaders} from '@angular/common/http';
 import {HttpClient} from '@angular/common/http'; //hhtp client object helps with all WEBApi methods
 import { Observable } from 'rxjs'; // to work or load single component not whole page or app.
 
-
 @Injectable({
   providedIn: 'root'
 })
+
 export class SeatserviceService {
   url = 'http://localhost:16553/api/bus/seatdetails';
 
@@ -15,13 +15,7 @@ export class SeatserviceService {
 
   constructor(private httpclient:HttpClient) { }
 
-  getseats(busId:number):Observable<any>{
-  
-    console.log(busId);
-    return this.httpclient.get<any[]>(this.url + '/' + busId)
-    
+  getseats(busId:number):Observable<any> {
+    return this.httpclient.get<any[]>(this.url + '/' + busId)  
   }
- 
-
-  
 }
