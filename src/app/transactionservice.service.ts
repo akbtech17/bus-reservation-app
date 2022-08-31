bdf7295c7ccee2024fd4aa271c97e287591ac16c
anshul bansal <akbt.tech17@gmail.com>
2022-09-01 03:35:53 +0530

    implemented error handling for admin, customer, seat, transaction services

--- src/app/transactionservice.service.ts
+++ src/app/transactionservice.service.ts
@@ -5,6 +5,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Ipassengerdet } from './ipassengerdet';
 import { ICreatetransactionrequest } from './icreatetransactionrequest';
+import { catchError, throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -13,24 +15,31 @@ import { ICreatetransactionrequest } from './icreatetransactionrequest';
 export class TransactionserviceService {  
 
   url='http://localhost:16553/api/transaction'
-
   httpOptions=  {headers: new HttpHeaders({'Content-type':'application/json'})}
 
-    constructor(private httpclient:HttpClient) { }
+  constructor(private httpclient:HttpClient) { }
     
-  getTransactiondet(customerId:number):Observable<Itransactionres>{
-    return this.httpclient.get<Itransactionres>(this.url+'/'+customerId)
+  getTransactiondet(customerId:number):Observable<Itransactionres> {
+    return this.httpclient.get<Itransactionres>(this.url+'/'+customerId).pipe(catchError(this.handleError))
   }
 
-  getPassenger(tId:number):Observable<Ipassengerdet>{
-    return this.httpclient.get<Ipassengerdet>(this.url + '/passengers/' + tId)
+  getPassenger(tId:number):Observable<Ipassengerdet> {
+    return this.httpclient.get<Ipassengerdet>(this.url + '/passengers/' + tId).pipe(catchError(this.handleError))
   }
   
   createTransaction(req : ICreatetransactionrequest):Observable<any> {
-      return this.httpclient.post<any>(this.url,req, this.httpOptions)
+      return this.httpclient.post<any>(this.url,req, this.httpOptions).pipe(catchError(this.handleError))
   }
 
   cancelTransaction(tId:number):Observable<any> {
-    return this.httpclient.delete<any>(this.url+'/'+tId)
+    return this.httpclient.delete<any>(this.url+'/'+tId).pipe(catchError(this.handleError))
+  }
+
+  // method to handle errors in client side
+  handleError(error: HttpErrorResponse) {
+    let errormessage = ''
+    errormessage = error.status+'\n'+error.statusText+'\n'+error.error
+    alert(errormessage)
+    return throwError(errormessage);
   }
 }
